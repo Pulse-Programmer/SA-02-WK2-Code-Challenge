c871f30a837967f8643be037dd6bcc5d5e501f0e
Stephen Ochieng <materiamystic@gmail.com>
2024-04-01 13:28:37 +0300

    Adds challenge 3 solution

--- solution3.js
+++ solution3.js
@@ -1,3 +1,18 @@
 function primeNum(numArr) {
-  numArr.map(number);
+  //conditional check on each number on the array to filter out prime numbers
+  return numArr.filter(
+    (number) =>
+      number === 2 ||
+      number === 3 ||
+      number === 5 ||
+      number === 7 ||
+      (number % 2 !== 0 &&
+        number % 3 !== 0 &&
+        number % 5 !== 0 &&
+        number % 7 !== 0 &&
+        number !== 1),
+  );
 }
+
+//test code
+console.log(primeNum([1, 2, 3, 4, 5, 6, 17, 27, 37]));
